863becb65fca0e0901401e58487fd15915080dd8
Shubhu09 <shubhanshu.markam123@gmail.com>
2022-12-15 23:09:17 +0530

    Java Pattern Programs

---
+++ Pattern/InvertedHalfPyramid.java
@@ -0,0 +1,12 @@
+package Pattern;
+
+public class InvertedHalfPyramid {
+    public static void main(String args[]){
+        for(int i=4;i>=1;i--){
+            for(int j=i;j>=1;j-- ){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
